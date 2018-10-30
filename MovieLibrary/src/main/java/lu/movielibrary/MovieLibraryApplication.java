5232ca55d4c55da7de6fb16e55c5263cb0562379
Hamza Torjmen <Hamza Torjmen@DESKTOP-95GOTOV>
2018-10-30 15:30:08 +0100

    1st commit

---
+++ MovieLibrary/src/main/java/lu/movielibrary/MovieLibraryApplication.java
@@ -0,0 +1,12 @@
+package lu.movielibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieLibraryApplication.class, args);
+	}
+}
